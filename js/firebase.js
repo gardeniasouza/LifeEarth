8fac8a9f97446d3cac40043e1c2ccd4fce1973d4
Gardênia Souza <gardeniasouza00@gmail.com>
2021-11-03 23:29:29 -0300

    Primeiro commit

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAz6cX2qRQRGh-kQsKIyKDwokD3pOFH7W0",
+    authDomain: "unifg-lifeearth.firebaseapp.com",
+    projectId: "unifg-lifeearth",
+    storageBucket: "unifg-lifeearth.appspot.com",
+    messagingSenderId: "982418968686",
+    appId: "1:982418968686:web:2d3343c4f13d756d72167f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
